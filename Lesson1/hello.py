8860ddc45499643c5f1f6ca8225b66a03d0a98ee
Lindsey Bolser <bolserl@mail.nmc.edu>
2021-01-20 05:28:44 -0500

    Initial Commit

---
+++ Lesson1/hello.py
@@ -0,0 +1,7 @@
+msg1 = "I Love CIT228"
+msg2 = "Python is Awesome!"
+print(msg1)
+print(msg2)
+
+import this
+print(this)
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,45 @@
+# exercise 1 - Lindsey Bolser
+print("------------------------------")
+print("Exercise 1")
+print(" ")
+name = "lindsey bolser"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ", name[0].upper())
+
+print(" ")
+# exercise 2
+print("------------------------------")
+print("Excercise 2")
+print(" ")
+noun = "cat"
+adj = "crazy"
+verb = "ran"
+ending = "across my keyboard."
+sentence1 = "The " + adj + " black " + noun + " " + verb + " " + ending
+sentence2 =f"The {adj} black {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+print(" ")
+# excercise 3
+print("------------------------------")
+print("Excercise 3")
+hope = """
+Hope, like the gleaming taper's light, 
+     Adorns and cheers our way; 
+And still, as darker grows the night, 
+     Emits a brighter ray."""
+print(hope)
+
+print(" ")
+# excercise 4
+print("------------------------------")
+print("Excercise 4")
+print(" ")
+print("Colors\t\tNumbers")
+print("------\t\t-------")
+print("Red\t\tOne")
+print("Green\t\tTwo")
+print("Blue\t\tThree")
