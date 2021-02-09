1c07fd42003d895b4691f4f654695df27161e023
Lindsey Bolser <bolserl@mail.nmc.edu>
2021-02-08 22:29:10 -0500

    Chapter8

---
+++ Chapter8/album.py
@@ -0,0 +1,24 @@
+print("\n<<<<< 8-7 >>>>>")
+def make_album(artist, albumName, tracks = None):
+
+    album = {}
+    album["Artist"] = artist.title()
+    album["Album Name"] = albumName.title()
+    if tracks:
+        album["Number of Tracks"] = tracks
+    return album
+
+print(make_album("Disturbed","Ten Thousand Fists"))
+print(make_album("Deviations Project", "Ivory Bow", 17))
+print(make_album("The Brian Setzer Orchestra", "The Dirty Boogie", 13))
+
+print("\n<<<<< 8-8 >>>>>")
+numToEnter = int(input("How many albums are you entering?"))
+counter = 0
+while counter < numToEnter:
+    artist = input("Artist: ")
+    albumName = input("Album: ")
+    tracks = int(input("Number of Tracks: "))
+    print(make_album(artist,albumName,tracks))
+    
+
---
+++ Chapter8/shirt_factory.py
@@ -0,0 +1,8 @@
+def make_shirt(size="Large", message="I LOVE Python!"):
+    print(f"Your shirt size is {size} with the message '{message}' printed on the front.")
+
+make_shirt("Small","This is Positional!")
+make_shirt(message="This is Keywords!",size="Medium")
+make_shirt(size="XL")
+make_shirt(message="Hello World!")
+make_shirt(size="XS",message="I am Different!")
